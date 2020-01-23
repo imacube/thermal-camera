f6ef10d45b954d72f685b5190d53f9372fc26177
Ryan <imacube@gmail.com>
2020-01-23 12:05:46 -0800

    Totally hacked together

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 import time
 
 from base_camera import BaseCamera
+from thermal import Thermal
 
 
 class Camera(BaseCamera):
@@ -8,8 +9,12 @@ class Camera(BaseCamera):
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
     imgs = [open(x, 'rb').read() for x in ['hues.png', 'hues2.png']]
 
+    thermal = Thermal()
+
     @staticmethod
     def frames():
         while True:
             time.sleep(1)
-            yield Camera.imgs[int(time.time()) % 2]
+
+            yield Camera.thermal.get_data()
+            # yield Camera.imgs[int(time.time()) % 2]
