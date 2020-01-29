40d926632c7a5348ead87741559314c09b732c15
Ryan <ryan@imacube.net>
2020-01-29 12:06:18 -0800

    Commenting out unused line that is looking for missing files

--- camera.py
+++ camera.py
@@ -7,7 +7,7 @@ from thermal import Thermal
 class Camera(BaseCamera):
     """An emulated camera implementation that streams a repeated sequence of
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
-    imgs = [open(x, 'rb').read() for x in ['hues.png', 'hues2.png']]
+    # imgs = [open(x, 'rb').read() for x in ['hues.png', 'hues2.png']]
 
     thermal = Thermal()
 
